2da2181eb04222720578f4d1415ff4ee03a64e1e
jessica <papylotomba@yahoo.fr>
2020-03-07 17:49:49 -0500

    create loop for testTime function, and event listener for local storage

--- script.js
+++ script.js
@@ -170,3 +170,22 @@ function testTime() {
         $(".form5").addClass("present");
     };
 }
+
+testTime();
+// Loops through input area to get item from local storage
+var x = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+// Test loop:
+for (var i = 0; i < x.length; i++) {
+    var dataHour = localStorage.getItem(x[i]);
+    // form - control
+    $(".form" + x[i]).val(dataHour);
+}
+// Event listener to save to local stroage
+$(".saveBtn").click(function () {
+    event.preventDefault();
+    var formValue = $(this).siblings(".form-control").val();
+    console.log("This worked");
+    var listItem = $(this).parent().data("hour");
+
+    localStorage.setItem(listItem, formValue);
+});
